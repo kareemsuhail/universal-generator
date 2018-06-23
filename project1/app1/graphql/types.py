50e2d300e4430da0bb18dc1a9652ab7a1d0f5fbc
kareem ayesh <1kayesh@students.iugaza.edu.ps>
2018-06-23 18:51:00 +0300

    remove example project files

--- project1/app1/graphql/types.py
+++
@@ -1,6 +0,0 @@
-from graphene_django import DjangoObjectType
-import graphene
-from .models_file.Student import Student
-class StudentType(DjangoObjectType):
-	class Meta:
-		model=Student
--- project1/app1/models.py
+++
@@ -1,2 +0,0 @@
-from django.db import models
-from .models_files.Student import Student
